220a3450971af0d7029488d1927e5fef253511c1
kategomezny <35971954+kategomezny@users.noreply.github.com>
2018-02-23 18:27:20 -0500

    Update task_01.py

--- task_01.py
+++ task_01.py
@@ -4,6 +4,28 @@
 
 
 def know_what_i_mean(wink, numwink=2):
+    """Returns the number of times that you mean.
+    
+    
+    Args:
+        wink(str):  Arg to be multiplied by numwik.
+        numwink(int):  Arg to multiply wink.
+        
+        
+    Returns:
+        str:  'Know what I mean?' plus wink multiplied by
+        numwink, plus nudges multiplied by numwink.
+        
+        
+    Examples:
+    
+        >>>know_what_i_mean('kt', 4)
+        'Know what I mean? ktktktkt, nudge nudge nudge nudge'
+        
+        >>> know_what_i_mean('a')
+        'Know what I mean? aa, nudge nudge'
+    """
+
     winks = (wink * numwink).strip()
     nudges = ('nudge ' * numwink).strip()
     retstr = 'Know what I mean? {}, {}'.format(winks, nudges)
